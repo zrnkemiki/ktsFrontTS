1f312cd24c379f251a72c1419b6c66fa940e03c7
zrnke <zrnkemiki@gmail.com>
2019-09-06 04:10:45 +0200

    bug fix

--- ktsnvtfront/src/app/app.module.ts
+++ ktsnvtfront/src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpErrorInterceptor } from './interceptors/http-error-interceptor';
 import { GenericService } from './service/generic.service';
-import { JwtInterceptor } from './interceptors/jwt-interceptor';
 import { ToastrModule } from 'ngx-toastr';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -28,8 +26,6 @@ import { HomepageComponent } from './homepage/homepage.component';
     ToastrModule.forRoot({preventDuplicates: true})
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
     GenericService,
     { provide: 'BASE_API_URL', useValue: 'http://localhost:8080/api' }
   ],
